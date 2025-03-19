a64e6eef073a090452f19478078cd364103f711f
vlastroG <vlastroganov@gmail.com>
2025-03-19 16:36:47 +0300

    Add pokemon info search

--- src/basic/pokemon-info-search/script.js
+++ src/basic/pokemon-info-search/script.js
@@ -11,6 +11,7 @@ const defenseElement = document.getElementById("defense");
 const specialAttackElement = document.getElementById("special-attack");
 const specialDefenseElement = document.getElementById("special-defense");
 const speedElement = document.getElementById("speed");
+const spriteElement = document.getElementById("sprite");
 
 const clearData = () => {
     nameElement.textContent = "";
@@ -24,35 +25,56 @@ const clearData = () => {
     specialAttackElement.textContent = "";
     specialDefenseElement.textContent = "";
     speedElement.textContent = "";
-}
-
-const searchPokemonById = (id) => {
-
-}
-
-const searchPokemonByName = (str) => {
-
+    spriteElement.style.display = "none";
 }
 
 const displayPokemonData = (pokemon) => {
     if (pokemon !== undefined) {
-        alert("Pokémon not found");
+        nameElement.textContent = pokemon.name.toUpperCase();
+        idElement.textContent = pokemon.id;
+        weightElement.textContent = pokemon.weight;
+        heightElement.textContent = pokemon.height;
+        typesElement.innerHTML = pokemon.types.map(t => `<div class="type-item">${t.type.name.toUpperCase()}</div>`).join("");
+        const stats = Object.fromEntries(pokemon.stats.map(t => [t.stat.name, t.base_stat]));
+        hpElement.textContent = stats.hp;
+        attackElement.textContent = stats.attack;
+        defenseElement.textContent = stats.defense;
+        specialAttackElement.textContent = stats["special-attack"];
+        specialDefenseElement.textContent = stats["special-defense"];
+        speedElement.textContent = stats.speed;
+        spriteElement.style.display = "block";
+        spriteElement.setAttribute("src", pokemon.sprites.front_default);
     } else {
+        alert("Pokémon not found");
         clearData();
     }
 }
 
-const searchPokemon = (str) => {
+const searchPokemon = async (str) => {
     const id = parseInt(str);
+    let searchStr;
     if (!isNaN(id)) {
-        displayPokemonData(searchPokemonById());
+        searchStr = id;
     } else {
-        displayPokemonData(searchPokemonByName(str));
+        searchStr = str
+            .trim()
+            .toLowerCase()
+            .replace(/[^a-z0-9\- ]/g, '')
+            .split(' ')
+            .map(s => s.trim())
+            .filter(s => s !== '')
+            .join('-');
+    }
+    try {
+        const res = await fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${searchStr}`);
+        const pokemon = await res.json();
+        displayPokemonData(pokemon);
+    } catch (err) {
+        displayPokemonData(undefined);
     }
 }
 
 searchBtn.addEventListener("click", () => {
-    console.log("clicked");
     if (!inputElement.value) {
         alert("Please enter Pokémon name or ID");
         clearData();
