f43181ef949e9b316130c3fec4fafba7a06350e1
vlastroG <vlastroganov@gmail.com>
2025-07-10 22:36:58 +0300

    Add props

--- src/react/travel-journal/src/App.jsx
+++ src/react/travel-journal/src/App.jsx
@@ -1,11 +1,31 @@
 import Header from "./components/Header.jsx";
 import Entry from "./components/Entry.jsx";
+import fujiImg from "./assets/japan.png";
+import sydneyImg from "./assets/sydney.png";
+import norwayImg from "./assets/norway.png";
 
 function App() {
     return (
         <>
             <Header/>
-            <Entry/>
+            <Entry entryImg={fujiImg}
+                   entryImgAlt="Mount Fuji"
+                   country="Japan"
+                   locationName="Mount Fuji"
+                   dates="12 Jan, 2023 - 24 Jan, 2023"
+                   locationDescription="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."/>
+            <Entry entryImg={sydneyImg}
+                   entryImgAlt="Sydney Opera House"
+                   country="Australia"
+                   locationName="Sydney Opera House"
+                   dates="27 May, 2023 - 8 Jun, 2023"
+                   locationDescription="The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings."/>
+            <Entry entryImg={norwayImg}
+                   entryImgAlt="Geirangerfjord"
+                   country="Norway"
+                   locationName="Geirangerfjord"
+                   dates="01 Oct, 2024 - 18 Nov, 2024"
+                   locationDescription="The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."/>
         </>
     )
 }
--- src/react/travel-journal/src/components/Entry.jsx
+++ src/react/travel-journal/src/components/Entry.jsx
@@ -1,30 +1,23 @@
 import marker from '../assets/marker.svg';
-import entryImg from '../assets/japan.png';
 import './entry.css'
 
 
-function Entry() {
+function Entry(props) {
     return (
-        <>
-            <div className="entry">
-                <div className="entry-img">
-                    <img src={entryImg} alt="Mount Fuji, Japan"/>
-                </div>
-                <div className="entry-info">
-                    <div className="location-header">
-                        <img src={marker} alt="Marker on the map"/>
-                        <h2 className="country">Japan</h2>
-                        <a href="" target="_blank" className="googlemaps-link">View on Google Maps</a>
-                    </div>
-                    <h3 className="location-name">Mount Fuji</h3>
-                    <span className="travel-dates">12 Jan, 2023 - 24 Jan, 2023</span>
-                    <p className="travel-description">Mount Fuji is the tallest mountain in Japan, standing at 3,776
-                        meters
-                        (12,380 feet). Mount Fuji is the
-                        single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
-                </div>
+        <article className="entry">
+            <div className="entry-img-container">
+                <img src={props.entryImg} alt={props.entryImgAlt}/>
             </div>
-        </>
+            <div className="entry-info">
+                <div className="location-header">
+                    <img src={marker} alt="Marker on the map"/>
+                    <h2 className="country">{props.country}</h2>
+                    <a href="" target="_blank" className="googlemaps-link">View on Google Maps</a>
+                </div>
+                <h3 className="location-name">{props.locationName}</h3>
+                <span className="travel-dates">{props.dates}</span>
+                <p className="travel-description">{props.locationDescription}</p></div>
+        </article>
     );
 }
 
