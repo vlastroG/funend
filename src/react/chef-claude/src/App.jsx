bd6f3c501a610e0f7c46d59bf010e4f9ac2826d2
vlastroG <vlastroganov@gmail.com>
2025-07-16 22:23:26 +0300

    Add form

--- src/react/chef-claude/src/App.jsx
+++ src/react/chef-claude/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header.jsx";
+import Main from "./components/Main.jsx";
 
 function App() {
 
     return (
         <>
             <Header/>
+            <Main/>
         </>
     )
 }
---
+++ src/react/chef-claude/src/components/Main.jsx
@@ -0,0 +1,12 @@
+export default function Main() {
+    return (
+        <main>
+            <form>
+                <input type="text"
+                       placeholder="e.g. oregano"
+                       aria-label="Add ingredient"/>
+                <button type="button">Add ingredient</button>
+            </form>
+        </main>
+    )
+}
